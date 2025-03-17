f405ab471d55c942912bd157205bceaf77768d5f
tayderivera <taydemrivera@gnmail.com>
2025-03-17 17:47:50 -0600

    ya cambia el color de los cajones dependiendo del estado

--- src/app/Componentes/main/main.component.ts
+++ src/app/Componentes/main/main.component.ts
@@ -1,35 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+
 import { HeaderComponent } from "../header/header.component";
-import { FirestoreService } from '../../Servicios/firestore.service';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import {CajonesService} from '../../Servicios/cajones.service';
+import { Component } from '@angular/core';
 import { PlumaService } from '../../Servicios/pluma.service';
+import { CommonModule } from '@angular/common';
+interface Cajon {
+  id: string;
+  nombre: string;
+  estado: string;
+}
+
+
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent], 
+  imports: [HeaderComponent, CommonModule], 
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent implements OnInit {
-  // Arreglo de colores de los cajones
-  colorDiv: string[] = [];
+export class MainComponent{
+cajones:  Cajon[] = [];
 
-  // Arreglo de tipos de cajones
-  tipoCajones: string[] = [];
 
-  // Arreglo de nombres de los cajones
-  nombresCajones: string[] = [];
 
-  constructor(private firestoreServ: FirestoreService, private plumaService : PlumaService) {}
-
-  ngOnInit(): void {
-    this.firestoreServ.obtenerDatos().subscribe((datos: any[]) => {
-      if (datos.length > 0) {
-        this.nombresCajones = datos.map((cajon) => cajon.nombre); // Obtener nombres
-        this.colorDiv = datos.map((cajon) => this.getColorPorEstado(cajon.estado)); // Obtener colores
-        this.tipoCajones = datos.map((cajon) => cajon.tipo); // Obtener tipos
-      }
+  constructor( private plumaService : PlumaService, private cajonesService: CajonesService){
+  }
+  ngOnInit() {
+    this.cajonesService.obtenerCajones().subscribe(data => {
+      this.cajones = data;
     });
   }
-
+  
   // Función para obtener el color según el estado
   getColorPorEstado(estado: string): string {
     switch (estado.toLowerCase()) {
@@ -44,10 +45,7 @@ export class MainComponent implements OnInit {
     }
   }
 
-  // Función para verificar si el cajón es de tipo "discapacidad"
-  esDiscapacidad(tipo: string): boolean {
-    return tipo === 'discapacidad';
-  }
+   
 
  //apertura de las plumas
  abrirPluma() {
